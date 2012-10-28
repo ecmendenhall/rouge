fdd73a187d3ce2933bb3c2e18e96f11d39fc2bef
Arlen Christian Mart Cuss <ar@len.me>
2012-10-28 23:11:20 +1100

    Refactor printer specs.

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -3,187 +3,182 @@ require 'spec_helper'
 require 'rouge'
 
 describe Rouge::Printer do
-  describe "the print method" do
-    it "should print numbers" do
-      Rouge.print(12755, "").should eq "12755"
+  describe ".print" do
+    context "numbers" do
+      it { Rouge.print(12755, "").should eq "12755" }
     end
 
-    it "should print symbols" do
-      Rouge.print(Rouge::Symbol[:loki], "").should eq "loki"
-      Rouge.print(Rouge::Symbol[:/], "").should eq "/"
-      Rouge.print(Rouge::Symbol[:wah?], "").should eq "wah?"
-      Rouge.print(Rouge::Symbol[:"!ruby!"], "").should eq "!ruby!"
-      Rouge.print(Rouge::Symbol[:nil], "").should eq "nil"
-      Rouge.print(Rouge::Symbol[:true], "").should eq "true"
-      Rouge.print(Rouge::Symbol[:false], "").should eq "false"
+    context "symbols" do
+      it { Rouge.print(Rouge::Symbol[:loki], "").should eq "loki" }
+      it { Rouge.print(Rouge::Symbol[:/], "").should eq "/" }
+      it { Rouge.print(Rouge::Symbol[:wah?], "").should eq "wah?" }
+      it { Rouge.print(Rouge::Symbol[:"!ruby!"], "").should eq "!ruby!" }
+      it { Rouge.print(Rouge::Symbol[:nil], "").should eq "nil" }
+      it { Rouge.print(Rouge::Symbol[:true], "").should eq "true" }
+      it { Rouge.print(Rouge::Symbol[:false], "").should eq "false" }
     end
 
-    describe "keywords" do
-      it "should print plain keywords" do
-        Rouge.print(:loki, "").should eq ":loki"
-        Rouge.print(:/, "").should eq ":/"
-        Rouge.print(:wah?, "").should eq ":wah?"
-        Rouge.print(:nil, "").should eq ":nil"
-        Rouge.print(:true, "").should eq ":true"
-        Rouge.print(:false, "").should eq ":false"
+    context "keywords" do
+      context "plain keywords" do
+        it { Rouge.print(:loki, "").should eq ":loki" }
+        it { Rouge.print(:/, "").should eq ":/" }
+        it { Rouge.print(:wah?, "").should eq ":wah?" }
+        it { Rouge.print(:nil, "").should eq ":nil" }
+        it { Rouge.print(:true, "").should eq ":true" }
+        it { Rouge.print(:false, "").should eq ":false" }
       end
 
-      it "should print string-symbols" do
-        Rouge.print(:"!ruby!", "").should eq ":\"!ruby!\""
-      end
-    end
-
-    describe "strings" do
-      it "should print plain strings" do
-        Rouge.print("akashi yo", "").should eq "\"akashi yo\""
-        Rouge.print("akashi \n woah!", "").should eq "\"akashi \\n woah!\""
-      end
-
-      it "should print escape sequences" do
-        Rouge.print("here \" goes", "").should eq "\"here \\\" goes\""
-        Rouge.print("here \\ goes", "").should eq "\"here \\\\ goes\""
-        Rouge.print("\a\b\e\f\n", "").should eq "\"\\a\\b\\e\\f\\n\""
-        Rouge.print("\r\t\v", "").should eq "\"\\r\\t\\v\""
+      context "string-symbols" do
+        it { Rouge.print(:"!ruby!", "").should eq ":\"!ruby!\"" }
       end
     end
 
-    describe "lists" do
-      it "should print the empty list" do
-        Rouge.print(Rouge::Seq::Cons[], "").should eq "()"
+    context "strings" do
+      context "plain strings" do
+        it { Rouge.print("akashi yo", "").should eq "\"akashi yo\"" }
+        it { Rouge.print("akashi\nwoah!", "").should eq "\"akashi\\nwoah!\"" }
       end
 
-      it "should print one-element lists" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:tiffany]], "").
-            should eq "(tiffany)"
-        Rouge.print(Rouge::Seq::Cons[:raaaaash], "").
-            should eq "(:raaaaash)"
-      end
-
-      it "should print multiple-element lists" do
-        Rouge.print(Rouge::Seq::Cons[1, 2, 3], "").should eq "(1 2 3)"
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:true],
-                                Rouge::Seq::Cons[], [], "no"], "").
-            should eq "(true () [] \"no\")"
-      end
-
-      it "should print nested lists" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Seq::Cons[Rouge::Seq::Cons[3],
-                    Rouge::Seq::Cons[Rouge::Seq::Cons[]]], 9,
-                    Rouge::Seq::Cons[Rouge::Seq::Cons[8], Rouge::Seq::Cons[8]]], "").
-            should eq "(((3) (())) 9 ((8) (8)))"
+      context "escape sequences" do
+        it { Rouge.print("here \" goes", "").should eq "\"here \\\" goes\"" }
+        it { Rouge.print("here \\ goes", "").should eq "\"here \\\\ goes\"" }
+        it { Rouge.print("\a\b\e\f\n", "").should eq "\"\\a\\b\\e\\f\\n\"" }
+        it { Rouge.print("\r\t\v", "").should eq "\"\\r\\t\\v\"" }
       end
     end
 
-    describe "vectors" do
-      it "should print the empty vector" do
-        Rouge.print([], "").should eq "[]"
+    context "lists" do
+      context "empty list" do
+        it { Rouge.print(Rouge::Seq::Cons[], "").should eq "()" }
       end
 
-      it "should print one-element vectors" do
-        Rouge.print([Rouge::Symbol[:tiffany]], "").should eq "[tiffany]"
-        Rouge.print([:raaaaash], "").should eq "[:raaaaash]"
+      context "one-element lists" do
+        it { Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:tiffany]], "").
+                 should eq "(tiffany)" }
+        it { Rouge.print(Rouge::Seq::Cons[:raaaaash], "").
+                 should eq "(:raaaaash)" }
       end
 
-      it "should print multiple-element vectors" do
-        Rouge.print([1, 2, 3], "").should eq "[1 2 3]"
-        Rouge.print([Rouge::Symbol[:true], Rouge::Seq::Cons[], [], "no"], "").
-            should eq "[true () [] \"no\"]"
+      context "multiple-element lists" do
+        it { Rouge.print(Rouge::Seq::Cons[1, 2, 3], "").should eq "(1 2 3)" }
+        it { Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:true],
+                                          Rouge::Seq::Cons[], [], "no"], "").
+                 should eq "(true () [] \"no\")" }
       end
 
-      it "should print nested vectors" do
-        Rouge.print([[[3], [[]]], 9, [[8], [8]]], "").
-            should eq "[[[3] [[]]] 9 [[8] [8]]]"
-      end
-    end
-
-    describe "quotations" do
-      it "should print (QUOTE X) as 'X" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:quote], Rouge::Symbol[:x]], "").
-            should eq "'x"
-      end
-
-      it "should print (QUOTE (QUOTE ((QUOTE X)))) as ''('X)" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:quote],
-                    Rouge::Seq::Cons[Rouge::Symbol[:quote],
-                    Rouge::Seq::Cons[Rouge::Seq::Cons[Rouge::Symbol[:quote],
-                    Rouge::Symbol[:x]]]]], "").
-            should eq "''('x)"
+      context "nested lists" do
+        it { Rouge.print(
+                 Rouge::Seq::Cons[
+                     Rouge::Seq::Cons[Rouge::Seq::Cons[3],
+                                      Rouge::Seq::Cons[Rouge::Seq::Cons[]]],
+                     9,
+                     Rouge::Seq::Cons[Rouge::Seq::Cons[8],
+                                      Rouge::Seq::Cons[8]]],
+                 "").
+                 should eq "(((3) (())) 9 ((8) (8)))" }
       end
     end
 
-    describe "vars" do
-      it "should print (VAR X/Y) as #'X/Y" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:var],
-                                Rouge::Symbol[:"x/y"]], "").
-            should eq "#'x/y"
+    context "vectors" do
+      context "the empty vector" do
+        it { Rouge.print([], "").should eq "[]" }
       end
 
-      it "should print (VAR (VAR ((VAR X/Y)))) as #'#'(#'X/Y)" do
-        Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:var],
-                    Rouge::Seq::Cons[Rouge::Symbol[:var],
-                    Rouge::Seq::Cons[Rouge::Seq::Cons[Rouge::Symbol[:var],
-                    Rouge::Symbol[:"x/y"]]]]], "").
-            should eq "#'#'(#'x/y)"
+      context "one-element vectors" do
+        it { Rouge.print([Rouge::Symbol[:tiffany]], "").should eq "[tiffany]" }
+        it { Rouge.print([:raaaaash], "").should eq "[:raaaaash]" }
       end
 
-      it "should print the var #'X/Y itself as #'X/Y" do
-        Rouge.print(Rouge::Var.new(:x, :y), "").should eq "#'x/y"
+      context "multiple-element vectors" do
+        it { Rouge.print([1, 2, 3], "").should eq "[1 2 3]" }
+        it { Rouge.print([Rouge::Symbol[:true], Rouge::Seq::Cons[], [], "no"], "").
+                 should eq "[true () [] \"no\"]" }
+      end
+
+      context "nested vectors" do
+        it { Rouge.print([[[3], [[]]], 9, [[8], [8]]], "").
+                 should eq "[[[3] [[]]] 9 [[8] [8]]]" }
       end
     end
 
-    describe "maps" do
-      it "should print the empty map" do
-        Rouge.print({}, "").should eq "{}"
+    context "quotations" do
+      it { Rouge.print(
+               Rouge::Seq::Cons[Rouge::Symbol[:quote], Rouge::Symbol[:x]],
+               "").
+               should eq "'x" }
+      it { Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:quote],
+                       Rouge::Seq::Cons[Rouge::Symbol[:quote],
+                       Rouge::Seq::Cons[Rouge::Seq::Cons[Rouge::Symbol[:quote],
+                       Rouge::Symbol[:x]]]]], "").
+               should eq "''('x)" }
+    end
+
+    context "vars" do
+      it { Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:var],
+                                        Rouge::Symbol[:"x/y"]], "").
+               should eq "#'x/y" }
+
+      it { Rouge.print(Rouge::Seq::Cons[Rouge::Symbol[:var],
+                       Rouge::Seq::Cons[Rouge::Symbol[:var],
+                       Rouge::Seq::Cons[Rouge::Seq::Cons[Rouge::Symbol[:var],
+                       Rouge::Symbol[:"x/y"]]]]], "").
+               should eq "#'#'(#'x/y)" }
+
+      it { Rouge.print(Rouge::Var.new(:x, :y), "").should eq "#'x/y" }
+    end
+
+    context "maps" do
+      context "the empty map" do
+        it { Rouge.print({}, "").should eq "{}" }
       end
 
-      it "should print one-element maps" do
-        Rouge.print({Rouge::Symbol[:a] => 1}, "").should eq "{a 1}"
-        Rouge.print({"quux" => [Rouge::Symbol[:lambast]]}, "").
-            should eq "{\"quux\" [lambast]}"
+      context "one-element maps" do
+        it { Rouge.print({Rouge::Symbol[:a] => 1}, "").should eq "{a 1}" }
+        it { Rouge.print({"quux" => [Rouge::Symbol[:lambast]]}, "").
+                 should eq "{\"quux\" [lambast]}" }
       end
 
-      it "should print multiple-element maps" do
+      context "multiple-element maps" do
         # XXX(arlen): these tests rely on stable-ish Hash order
-        Rouge.print({:a => 1, :b => 2}, "").should eq "{:a 1, :b 2}"
-        Rouge.print({:f => :f, :y => :y, :z => :z}, "").
-            should eq "{:f :f, :y :y, :z :z}"
+        it { Rouge.print({:a => 1, :b => 2}, "").should eq "{:a 1, :b 2}" }
+        it { Rouge.print({:f => :f, :y => :y, :z => :z}, "").
+                 should eq "{:f :f, :y :y, :z :z}" }
       end
 
-      it "should print nested maps" do
+      context "nested maps" do
         # XXX(arlen): this test relies on stable-ish Hash order
-        Rouge.print({:a => {:z => 9},
-                    :b => {:q => Rouge::Symbol[:q]}}, "").
-            should eq "{:a {:z 9}, :b {:q q}}"
-        Rouge.print({{9 => 7} => 5}, "").should eq "{{9 7} 5}"
+        it { Rouge.print({:a => {:z => 9},
+                          :b => {:q => Rouge::Symbol[:q]}}, "").
+                 should eq "{:a {:z 9}, :b {:q q}}" }
+        it { Rouge.print({{9 => 7} => 5}, "").should eq "{{9 7} 5}" }
       end
     end
 
-    it "should print the fundamental objects" do
-      Rouge.print(nil, "").should eq "nil"
-      Rouge.print(true, "").should eq "true"
-      Rouge.print(false, "").should eq "false"
+    context "fundamental objects" do
+      it { Rouge.print(nil, "").should eq "nil" }
+      it { Rouge.print(true, "").should eq "true" }
+      it { Rouge.print(false, "").should eq "false" }
     end
 
-    it "should print Ruby classes in their namespace" do
-      Rouge.print(Object, "").should eq "ruby/Object"
-      Rouge.print(Class, "").should eq "ruby/Class"
-      Rouge.print(Rouge::Context, "").should eq "ruby/Rouge.Context"
-      anon = Class.new
-      Rouge.print(anon, "").should eq anon.inspect
+    context "Ruby classes" do
+      it { Rouge.print(Object, "").should eq "ruby/Object" }
+      it { Rouge.print(Class, "").should eq "ruby/Class" }
+      it { Rouge.print(Rouge::Context, "").should eq "ruby/Rouge.Context" }
+
+      let(:anon) { Class.new }
+      it { Rouge.print(anon, "").should eq anon.inspect }
     end
 
-    it "should print builtin forms in their namespace" do
-      Rouge.print(Rouge::Builtin[Rouge::Builtins.method(:let)], "").
-          should eq "rouge.builtin/let"
-      Rouge.print(Rouge::Builtin[Rouge::Builtins.method(:def)], "").
-          should eq "rouge.builtin/def"
+    context "builtin forms" do
+      it { Rouge.print(Rouge::Builtin[Rouge::Builtins.method(:let)], "").
+               should eq "rouge.builtin/let" }
+      it { Rouge.print(Rouge::Builtin[Rouge::Builtins.method(:def)], "").
+               should eq "rouge.builtin/def" }
     end
 
-    describe "unknown form behaviour" do
-      it "should print the Ruby inspection of unknown forms" do
-        l = lambda {}
-        Rouge.print(l, "").should eq l.inspect
-      end
+    context  "unknown forms" do
+      let(:l) { lambda {} }
+      it { Rouge.print(l, "").should eq l.inspect }
     end
   end
 end
