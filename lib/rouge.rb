6cc4d903d1c0b63f8de86c9be12e7c4b801de0bc
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-12-23 13:03:00 -0800

    Move start time to bin/rouge --time-startup

--- lib/rouge.rb
+++ lib/rouge.rb
@@ -4,11 +4,6 @@ if RUBY_VERSION < "1.9"
   STDERR.puts "Rouge will probably not run on anything less than Ruby 1.9."
 end
 
-module Rouge; end
-
-start = Time.now
-Rouge.define_singleton_method :start, lambda {start}
-
 module Rouge
   require 'rouge/version'
   require 'rouge/wrappers'
